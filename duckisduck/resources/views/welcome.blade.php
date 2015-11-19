96848c7a4976dd582af73768647406ffcfe6dd33
teaspoon2504@gmail.com <teaspoon2504>
2015-11-19 12:04:32 +0700

    dengan logo duck

---
+++ duckisduck/resources/views/welcome.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laravel</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="css/materialize-orange.css"  media="screen,projection"/>
+      <link type="text/css" rel="stylesheet" href="css/ganti.css" media="screen, projection"/>
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        
+    </head>
+    <body>
+        <!--       konten-->
+<div class="container">    
+<div class="row">
+    
+    <form class="col s4" id="tengah">
+        <div class="row">
+            <div class="center-align">
+                <img src="assets/logo.png" style="width: 150px">
+            </div>
+            <br/>
+            <div class="input-field">
+                <i class="material-icons prefix">account_circle</i>
+                <input id="icon_prefix" type="text" class="validate">
+                <label for="icon_prefix">Username</label>
+            </div>
+        </div>
+        <div class="row">
+            <div class="input-field">
+                <i class="material-icons prefix">vpn_key</i>
+                <input id="icon_telephone" type="text" class="validate">
+                <label for="icon_telephone">Password</label>
+            </div>
+        </div>
+        <div class="row center-align">
+            <div>
+            <a class="btn waves-effect waves-light" href="waiters.html">Log In</a>
+            </div>
+        </div>
+    </form>
+    <div class="col s4"></div>
+          </div>
+    </div>
+        
+        
+      <!--Import jQuery before materialize.js-->
+      <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+      <script type="text/javascript" src="js/materialize.min.js"></script>
+    </body>
+</html>
